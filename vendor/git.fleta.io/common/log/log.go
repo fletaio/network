9072bcb20374e60696316fd0083e27bd9f71c24b
judasowelu <judasowelu@firstchain.io>
2018-09-15 11:40:31 +0900

    log system refactoring

--- vendor/git.fleta.io/common/log/log.go
+++ vendor/git.fleta.io/common/log/log.go
@@ -3,7 +3,6 @@ package log
 import (
 	"context"
 	stdlog "log"
-	"time"
 
 	plog "github.com/go-playground/log"
 	"github.com/go-playground/log/handlers/console"
@@ -33,41 +32,30 @@ func init() {
 // you can set this to to enable testing (with coverage) of your Fatal() and Fatalf()
 // methods.
 func SetExitFunc(fn func(code int)) {
-	plog.ExitFunc = fn
+	plog.SetExitFunc(fn)
 }
 
 // SetWithErrorFn sets a custom WithError function handlers
 func SetWithErrorFn(fn func(plog.Entry, error) plog.Entry) {
-	plog.WithErrFn = fn
+	plog.SetWithErrorFn(fn)
 }
 
 // SetContext sets a log entry into the provided context
 func SetContext(ctx context.Context, e plog.Entry) context.Context {
-	return context.WithValue(ctx, plog.CtxIdent, e)
+	return plog.SetContext(ctx, e)
 }
 
 // GetContext returns the log Entry found in the context,
 // or a new Default log Entry if none is found
 func GetContext(ctx context.Context) plog.Entry {
-	v := ctx.Value(plog.CtxIdent)
-	if v == nil {
-		return plog.NewEntryWithFields(nil)
-	}
-	return v.(plog.Entry)
+	return plog.GetContext(ctx)
 }
 
 // HandleEntry handles the log entry and fans out to all handlers with the proper log level
 // This is exposed to allow for centralized logging whereby the log entry is marshalled, passed
 // to a central logging server, unmarshalled and finally fanned out from there.
 func HandleEntry(e plog.Entry) {
-	if !e.Start.IsZero() {
-		e = e.WithField("duration", time.Since(e.Start))
-	}
-	e.Timestamp = time.Now()
-
-	for _, h := range plog.LogHandlers[e.Level] {
-		h.Log(e)
-	}
+	plog.HandleEntry(e)
 }
 
 // F creates a new Field using the supplied key + value.
@@ -79,139 +67,113 @@ func F(key string, value interface{}) plog.Field {
 // AddHandler adds a new log handler and accepts which log levels that
 // handler will be triggered for
 func AddHandler(h Handler, levels ...plog.Level) {
-	for _, level := range levels {
-		handler := append(plog.LogHandlers[level], h)
-		plog.LogHandlers[level] = handler
-	}
+	plog.AddHandler(h, levels...)
 }
 
 // WithDefaultFields adds fields to the underlying logger instance
 func WithDefaultFields(fields ...plog.Field) {
-	plog.LogFields = append(plog.LogFields, fields...)
+	plog.WithDefaultFields(fields...)
 }
 
 // WithField returns a new log entry with the supplied field.
 func WithField(key string, value interface{}) plog.Entry {
-	ne := plog.NewEntryWithFields(plog.LogFields)
-	ne.Fields = append(ne.Fields, plog.Field{Key: key, Value: value})
-	return ne
+	return plog.WithField(key, value)
 }
 
 // WithFields returns a new log entry with the supplied fields appended
 func WithFields(fields ...plog.Field) plog.Entry {
-	ne := plog.NewEntryWithFields(plog.LogFields)
-	ne.Fields = append(ne.Fields, fields...)
-	return ne
+	return plog.WithFields(fields...)
 }
 
 // WithTrace withh add duration of how long the between this function call and
 // the susequent log
 func WithTrace() plog.Entry {
-	ne := plog.NewEntryWithFields(plog.LogFields)
-	ne.Start = time.Now()
-	return ne
+	return plog.WithTrace()
 }
 
 // WithError add a minimal stack trace to the log Entry
 func WithError(err error) plog.Entry {
-	ne := plog.NewEntryWithFields(plog.LogFields)
-	return plog.WithErrFn(ne, err)
+	return plog.WithError(err)
 }
 
 // Debug logs a debug entry
 func Debug(v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Debug(v...)
+	plog.Debug(v...)
 }
 
 // Debugf logs a debug entry with formatting
 func Debugf(s string, v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Debugf(s, v...)
+	plog.Debugf(s, v...)
 }
 
 // Info logs a normal. information, entry
 func Info(v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Info(v...)
+	plog.Info(v...)
 }
 
 // Infof logs a normal. information, entry with formatiing
 func Infof(s string, v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Infof(s, v...)
+	plog.Infof(s, v...)
 }
 
 // Notice logs a notice log entry
 func Notice(v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Notice(v...)
+	plog.Notice(v...)
 }
 
 // Noticef logs a notice log entry with formatting
 func Noticef(s string, v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Noticef(s, v...)
+	plog.Noticef(s, v...)
 }
 
 // Warn logs a warn log entry
 func Warn(v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Warn(v...)
+	plog.Warn(v...)
 }
 
 // Warnf logs a warn log entry with formatting
 func Warnf(s string, v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Warnf(s, v...)
+	plog.Warnf(s, v...)
 }
 
 // Panic logs a panic log entry
 func Panic(v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Panic(v...)
+	plog.Panic(v...)
 }
 
 // Panicf logs a panic log entry with formatting
 func Panicf(s string, v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Panicf(s, v...)
+	plog.Panicf(s, v...)
 }
 
 // Alert logs an alert log entry
 func Alert(v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Alert(v...)
+	plog.Alert(v...)
 }
 
 // Alertf logs an alert log entry with formatting
 func Alertf(s string, v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Alertf(s, v...)
+	plog.Alertf(s, v...)
 }
 
 // Fatal logs a fatal log entry
 func Fatal(v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Fatal(v...)
+	plog.Fatal(v...)
 }
 
 // Fatalf logs a fatal log entry with formatting
 func Fatalf(s string, v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Fatalf(s, v...)
+	plog.Fatalf(s, v...)
 }
 
 // Error logs an error log entry
 func Error(v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Error(v...)
+	plog.Error(v...)
 }
 
 // Errorf logs an error log entry with formatting
 func Errorf(s string, v ...interface{}) {
-	e := plog.NewEntryWithFields(plog.LogFields)
-	e.Errorf(s, v...)
+	plog.Errorf(s, v...)
 }
 
 // Handler is an interface that log handlers need to comply with
